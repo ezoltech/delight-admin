d8a123a703a549871f8d4faac467624ed3f19d07
Yohannes Tezera <52599607+YohannesTz@users.noreply.github.com>
2023-12-10 17:17:40 +0300

    attempt to route

--- src/pages/MainAdminPage.jsx
+++ src/pages/MainAdminPage.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Nav } from "../components/Nav";
 import { Outlet } from "react-router-dom";
+import ContentData from "./pages/contentData";
+import AdminData from "./pages/adminData";
 
 const MainAdminPage = () => {
+  const [route, setRoute] = useState("/content");
+
+  useEffect(() => {
+    // Code to fetch the current route from the server or browser history
+    // and update the route state variable
+    setRoute(window.location.pathname);
+    console.log(window.location.pathname);
+  }, []);
+
+  const pageContent = () => {
+    switch (route) {
+      case "/content":
+        return <ContentData />
+      case "/b":
+        return <AdminData />
+    }
+  };
+
   return (
     <div>
-      <Nav />
-      <Outlet />
+      <ul>
+        <li>Content Data <a onClick={setRoute("/a")} /></li>
+        <li>Contact Data<a onClick={setRoute("/b")} /></li>
+        <li>Service Data <a onClick={setRoute("/c")} /></li>
+        <li>Admin Data <a onClick={setRoute("/d")} /></li>
+      </ul>
+      {/*       <Navbar fluid rounded>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link href="/main/contents">content data</Navbar.Link>
+          <Navbar.Link href="/main/contact">contact data</Navbar.Link>
+          <Navbar.Link href="/main/services">services data</Navbar.Link>
+          <Navbar.Link href="/main/admin">admin data </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar> */}
     </div>
   );
 };
